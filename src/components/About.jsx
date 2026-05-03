4b84f929d6ec33bc6536f7b1ba22f78f112145dc
Ananya Darna <ananyadarna@gmail.com>
2026-05-03 19:26:57 +0530

    Fix profile image path

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,7 @@ export default function About() {
           <div className="about-image-wrap reveal" ref={imageRef}>
             <div className="about-glow" />
             <div className="about-image-frame">
-              <div className="about-image-inner"><img src="public\profile.jpeg" alt="Ananya Darna" style={{ 
+              <div className="about-image-inner"><img src="/profile.jpeg" alt="Ananya Darna" style={{ 
       width: '100%', 
       height: '100%', 
       objectFit: 'cover', 
